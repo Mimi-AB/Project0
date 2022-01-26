34ea98e58e9918f737ff4ef905d3c046389f7723
Mimi-AB <seunabimbola98@gmail.com>
2022-01-26 08:02:31 -0500

    Project 0 start

---
+++ src/main/java/com/revature/Person.java
@@ -0,0 +1,69 @@
+package com.revature;
+
+public class Person
+{
+    private String fName;
+    private String lName;
+    private String username;
+    private String password;
+    private String email;
+
+    public Person(String fName, String lName, String username, String password, String email)
+    {
+        this.fName = fName;
+        this.lName = lName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getfName()
+    {
+        return fName;
+    }
+
+    public void setfName(String fName)
+    {
+        this.fName = fName;
+    }
+
+    public String getlName()
+    {
+        return lName;
+    }
+
+    public void setlName(String lName)
+    {
+        this.lName = lName;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+}
